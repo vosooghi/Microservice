f75d570b587ea5d751e137cc55c2466d45f99b02
Abbas Vosoughi <abbas769@hotmail.com>
2023-11-12 10:53:36 +0330

    Rpc implemented.

---
+++ RabbitMqSample/RabbitMqSample.RPC.RpcCaller/Program.cs
@@ -0,0 +1,9 @@
+﻿using RabbitMqSample.RPC.RpcCaller;
+
+var rpcClient = new RpcClient();
+
+Console.WriteLine(" [x] Requesting fib(30)");
+var response = rpcClient.Call("30");
+
+Console.WriteLine(" [.] Got '{0}'", response);
+rpcClient.Close();
