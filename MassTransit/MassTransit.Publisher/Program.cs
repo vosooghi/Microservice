c3e53f08ee60727da63c6067f8305ac979a1c193
Abbas Vosoughi <abbas769@hotmail.com>
2023-11-12 13:16:03 +0330

    completed.

---
+++ MassTransit/MassTransit.Publisher/Program.cs
@@ -0,0 +1,24 @@
+﻿using MassTransit;
+using MassTransit.Contracts;
+
+var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
+{
+    sbc.Host("rabbitmq://localhost");
+});
+
+await bus.StartAsync();
+string result = "";
+do
+{
+    Console.WriteLine("Write your message ");
+    var message = new Message
+    {
+        Text = Console.ReadLine()
+    };
+    await bus.Publish(message);
+    Console.WriteLine("Continue messaging? y / anything else: ");
+    result = Console.ReadLine().ToLower();
+} while (result=="y");
+
+
+await bus.StopAsync();
---
+++ MassTransit/MassTransit.Subscriber/Program.cs
@@ -0,0 +1,22 @@
+﻿using MassTransit;
+using MassTransit.Contracts;
+
+var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
+{
+    sbc.Host("rabbitmq://localhost");
+
+    sbc.ReceiveEndpoint("test_queue", ep =>
+    {
+        ep.Handler<Message>(context =>
+        {
+            return Console.Out.WriteLineAsync($"Received: {context.Message.Text}");
+        });
+    });
+});
+
+await bus.StartAsync();
+
+Console.WriteLine("Press any key to exit");
+await Task.Run(() => Console.ReadKey());
+
+await bus.StopAsync();
