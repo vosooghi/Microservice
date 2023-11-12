2e61048c431b2edb81e5d1483696622c1c143e63
Abbas Vosoughi <abbas769@hotmail.com>
2023-11-12 10:43:25 +0330

    Fanout Exchange implemented.

---
+++ RabbitMqSample/RabbitMqSample.PubSub.Pub/Program.cs
@@ -0,0 +1,56 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMqSample.Core.Entities;
+using System.Data.Common;
+using RabbitMqSample.Toolkit;
+
+ConnectionFactory _connectionFactory;
+IConnection _connection;
+IModel _model;
+string QueueName;
+const string ExchangeName = "MyPaymentServiceExchange";
+
+CreateConnection();
+string key;
+do
+{
+
+    SendPayments(10);
+    Console.WriteLine("".PadLeft(100, '-'));
+    Console.WriteLine();
+
+    Console.WriteLine("do you want to send message:y/any thing else ");
+    key = Console.ReadLine();
+    Console.WriteLine();
+
+} while (key.ToLower() == "y");
+
+void CreateConnection()
+{
+    _connectionFactory = new ConnectionFactory
+    {
+        HostName = "localhost",
+        UserName = "guest",
+        Password = "guest",
+        Port = Protocols.DefaultProtocol.DefaultPort
+    };
+    _connection = _connectionFactory.CreateConnection();
+    _model = _connection.CreateModel();
+    _model.ExchangeDeclare(ExchangeName,ExchangeType.Fanout ,false);
+}
+
+void SendPayments(int paymentCount)
+{
+    for (int i = 0; i < paymentCount; i++)
+    {
+        Payment payment = new Payment
+        {
+            FirstName = $"FiratName {i}",
+            LastName = $"LastName {i}",
+            CardNumber = $"1111-2222-3333-{i.ToString().PadLeft(4, '0')}",
+            Value = Math.Pow(i / 3, 5)
+        };
+        _model.BasicPublish(ExchangeName, "", null, payment.ToByteArray());
+        Console.WriteLine($"Message Send for {i}");
+    }
+}
---
+++ RabbitMqSample/RabbitMqSample.PubSub.Sub/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMqSample.Core.Entities;
+using System.Data.Common;
+using RabbitMqSample.Toolkit;
+
+ConnectionFactory _connectionFactory;
+IConnection _connection;
+IModel _model;
+string QueueName;
+const string ExchangeName = "MyPaymentServiceExchange";
+
+    CreateConnection();
+
+
+    var consumer = new EventingBasicConsumer(_model);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var payment = body.FromByteArray<Payment>();
+        Console.WriteLine($" [x] Received {payment.FirstName} {payment.LastName} {payment.Value}");
+    };
+    _model.BasicConsume(queue: QueueName,
+                         autoAck: true,
+                         consumer: consumer);
+
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+
+void CreateConnection()
+{
+    _connectionFactory = new ConnectionFactory
+    {
+        HostName = "localhost",
+        UserName = "guest",
+        Password = "guest",
+        Port = Protocols.DefaultProtocol.DefaultPort
+    };
+    _connection = _connectionFactory.CreateConnection();
+    _model = _connection.CreateModel();
+    QueueName = _model.QueueDeclare().QueueName;
+    _model.QueueBind(QueueName, ExchangeName, "");
+
+}
