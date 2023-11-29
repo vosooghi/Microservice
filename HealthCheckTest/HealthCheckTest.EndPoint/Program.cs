83558dec6c9c76e6e5610dea6a7dcdb71ac1fa5c
Abbas Vosoughi <abbas769@hotmail.com>
2023-11-29 15:59:33 +0330

    Sample health check implemented.

---
+++ HealthCheckTest/HealthCheckTest.EndPoint/Program.cs
@@ -0,0 +1,29 @@
+using HealthCheckTest.EndPoint;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Health check middleware
+builder.Services.AddHealthChecks().AddCheck<TempHC>("HC");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.MapHealthChecks("/h");
+app.MapControllers();
+app.Run();
---
+++ HealthCheckTest/HealthCheckTest.EndPoint/TempHC.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.OpenApi.Models;
+
+namespace HealthCheckTest.EndPoint
+{
+    public class TempHC : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            //DB check
+            //Message Broker check
+            //Service x check
+            return Task.FromResult(HealthCheckResult.Healthy("Healthy"));
+        }
+    }
+}
